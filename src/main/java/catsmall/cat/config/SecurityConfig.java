65012b5c0f99d5c425b372cb7a349393cb7943e5
GyuHyeong <cjsworbehd13@gmail.com>
2021-03-26 15:50:24 +0900

    healthcheck기능 추가

--- src/main/java/catsmall/cat/config/SecurityConfig.java
+++ src/main/java/catsmall/cat/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .mvcMatchers("/", "/api/**","/board", "/item/**", "/search/**", "/profile",
+                .mvcMatchers("/", "/api/**","/board", "/item/**", "/search/**", "/profile", "/application/health",
                         "/members/signin", "/members/signup", "/members/logout", "/members/forgot/**").permitAll()
                 .mvcMatchers("/admin/**","/admin").hasRole("ADMIN")
                 .anyRequest().authenticated();
